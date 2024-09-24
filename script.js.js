da5ddcd0737e2f12a69beb54bb7de0f556869e4e
Moshiur314 <74732491+Moshiur314@users.noreply.github.com>
2024-09-24 21:46:55 +0600

    Add files via upload

---
+++ script.js.js
@@ -0,0 +1,5 @@
+document.getElementById('startJourney').addEventListener('click', function() {
+    document.querySelector('.background').style.display = 'none';
+    document.getElementById('hiddenContent').style.display = 'block';
+  });
+  
